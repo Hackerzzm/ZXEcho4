b53f85a776a99bd84536eb7c9353dc661e47d11d
Hackerzzm <zzmhacker@gmail.com>
2017-12-19 10:51:23 +0800

    调试备份

---
+++ OpenVideoCall/src/main/java/io/agora/openvcall/model/EngineConfig.java
@@ -0,0 +1,16 @@
+package io.agora.openvcall.model;
+
+public class EngineConfig {
+  public int mVideoProfile;
+
+  public int mUid;
+
+  public String mChannel;
+
+  EngineConfig() {
+  }
+
+  public void reset() {
+    mChannel = null;
+  }
+}
